f3c2e735ea246f2506b4de0f4ec7fa4ddb2ed4ed
nostalgia <skfk0135@gmail.com>
2020-07-15 16:58:57 +0900

    add user demand model

---
+++ src/models/user-demands.js
@@ -0,0 +1,19 @@
+export default (sequelize, DataTypes) => {
+  const UserDemand = sequelize.define('user-demand', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    minSize: { type: DataTypes.INTEGER, allowNull: false },
+    maxSize: { type: DataTypes.INTEGER, allowNull: false },
+    startDate: { type: DataTypes.DATE, allowNull: false },
+    endDate: { type: DataTypes.DATE, allowNull: false },
+    username: { type: DataTypes.STRING(20), allowNull: false },
+    companyName: { type: DataTypes.STRING(20) },
+    phoneNumber: { type: DataTypes.STRING(20) },
+    email: { type: DataTypes.STRING(30) },
+    description: { type: DataTypes.STRING(1024) }
+  }, {
+    sequelize,
+    timestamps: false
+  });
+
+  return UserDemand;
+}
