102e8f0ef39545fc6879e62f278656baec8ae7bd
nostalgia <skfk0135@gmail.com>
2020-07-20 20:25:40 +0900

    await 빠짐

--- src/resources/v1/auth/sign-up/controller.ts
+++ src/resources/v1/auth/sign-up/controller.ts
@@ -6,7 +6,7 @@ export function signUp (app: express.Application): void {
     app.post('/v1/auth/sign-up', async (req: express.Request, res: express.Response) => {
         try {
             validateRequest(req.body);
-            const tokenResponse = createUserAndBuildTokenResponse(req.body);
+            const tokenResponse = await createUserAndBuildTokenResponse(req.body);
             res.status(201).send(tokenResponse);
         } catch(e) {
             console.log(e);
