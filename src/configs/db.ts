0bfa5991a077eba030cfd01d14ec6ddc82144771
nostalgia <skfk0135@gmail.com>
2020-07-23 17:35:10 +0900

    replace config values with environment variables

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -1,23 +1,23 @@
 export default {
     "development": {
-        "username": "root",
-        "password": null,
-        "database": "sequelize",
-        "host": "127.0.0.1",
+        "username": process.env.DATABASE_USERNAME,
+        "password": process.env.DATABASE_PASSWORD,
+        "database": process.env.DATABASE_NAME,
+        "host": process.env.DATABASE_URL,
         "dialect": "mysql"
     },
     "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
+        "username": process.env.DATABASE_USERNAME,
+        "password": process.env.DATABASE_PASSWORD,
+        "database": process.env.DATABASE_NAME,
+        "host": process.env.DATABASE_URL,
         "dialect": "mysql"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
+        "username": process.env.DATABASE_USERNAME,
+        "password": process.env.DATABASE_PASSWORD,
+        "database": process.env.DATABASE_NAME,
+        "host": process.env.DATABASE_URL,
         "dialect": "mysql"
     }
 };
