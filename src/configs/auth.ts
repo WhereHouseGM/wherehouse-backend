d631de7c094898c01bdfe817927ecdfaab9c00ae
ageun <skfk0135@gmail.com>
2020-07-23 17:48:54 +0900

    Update auth.ts

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -3,6 +3,6 @@ export default {
         accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRESIN || "3h",
         refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRESIN || "4d",
         tokenType: process.env.TOKEN_TYPE || "Bearer",
-        secret: process.env.JWT_SECRET
+        secret: process.env.JWT_SECRET as string
     }
 };
