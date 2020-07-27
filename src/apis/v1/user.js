e9d3c2a97dba09b372b09cffbf2b84a2f681cbd6
nostalgia <skfk0135@gmail.com>
2020-07-27 19:29:40 +0900

    asdfsdaf

--- src/apis/v1/user.js
+++ src/apis/v1/user.js
@@ -1,6 +1,17 @@
 const { authorize } = require("../../middlewares/auth");
 const db = require("../../models");
 const HTTPError = require("node-http-error");
+const Joi = require("joi");
+
+const patchUserRequestValidator = Joi.object({
+	name: Joi.string().max(10),
+	email: Joi.string().email().max(30),
+	password: Joi.string().min(64).max(64),
+	type: Joi.string().valid("SHIPPER", "OWNER"),
+	phoneNumber: Joi.string().max(20),
+	telephoneNumber: Joi.string().max(20),
+	companyName: Joi.string().max(20)
+});
 
 module.exports = (router) => {
 	router.get("/users/:userId", authorize(), async function (req, res, next) {
@@ -26,4 +37,44 @@ module.exports = (router) => {
 			next(err);
 		}
 	});
+
+	router.patch("/users/:userId", authorize(), async function (req, res, next) {
+		try {
+			const userIdFromToken = res.locals.userId;
+			const userIdParam = parseInt(req.params.userId);
+
+			const user = await db.users.findByPk(userIdParam);
+
+			if(user === null) throw new HTTPError(404, "Not Found Error");
+			if(userIdFromToken !== userIdParam) throw new HTTPError(403, "Forbidden Error");
+
+			// validate request
+			const { value, error } = await patchUserRequestValidator.validate(req.body);
+
+			if(error) throw error;
+
+			// TODO 함수 분리하기
+			user.name = value.name || user.name;
+			user.email = value.email || user.email;
+			user.password = value.password || user.password;
+			user.type = value.type || user.type;
+			user.phoneNumber = value.phoneNumber || user.phoneNumber;
+			user.telephoneNumber = value.telephoneNumber || user.telephoneNumber;
+			user.companyName = value.companyName || user.companyName;
+
+			await user.save();
+
+			res.status(200).json({
+				id: user.id,
+				name: user.name,
+				email: user.email,
+				type: user.type,
+				telephoneNumber: user.telephoneNumber,
+				phoneNumber: user.phoneNumber,
+				companyName: user.companyName
+			});
+		} catch(err) {
+			next(err);
+		}
+	})
 };
--- src/test/patch-user.test.js
+++ src/test/patch-user.test.js
@@ -9,14 +9,17 @@ const { signUp } = require("./sign-up.test");
 chai.use(chaiHttp);
 chai.should();
 
-async function getUser(userId, tokenType, accessToken) {
+async function patchUser(userId, patchUserRequest, tokenType, accessToken) {
 	return chai.request(app)
-		.get(`/v1/users/${userId}`)
-		.set("Authorization", `${tokenType} ${accessToken}`);
+		.patch(`/v1/users/${userId}`)
+		.set("Authorization", `${tokenType} ${accessToken}`)
+		.send(patchUserRequest);
 }
 
-describe("get user", function() {
+describe("patch user", function() {
 	let signUpResponse;
+	let patchUserRequest = {};
+
 	before(async function() {
 		await db.sequelize.sync({ force: true });
 
@@ -34,19 +37,24 @@ describe("get user", function() {
 	});
 
 	it("should success", async function() {
-		const res = await getUser(signUpResponse.body.user.id, signUpResponse.body.tokenType, signUpResponse.body.accessToken);
+		patchUserRequest = {
+			name: "patchuser"
+		};
+
+		const res = await patchUser(signUpResponse.body.user.id, patchUserRequest, signUpResponse.body.tokenType, signUpResponse.body.accessToken);
 
 		expect(res.status).to.equal(200);
 		expect(res.body).not.to.be.empty;
 		expect(res.body.id).to.be.a("number");
 		expect(res.body.name).to.be.a("string");
+		expect(res.body.name).to.be.equal("patchuser");
 		expect(res.body.email).to.be.a("string");
 		expect(res.body.type).to.be.a("string");
 		expect(res.body.phoneNumber).to.be.a("string");
 	});
 
 	it("should fail due to wrong access token", async function() {
-		const res = await getUser(signUpResponse.body.user.id, signUpResponse.body.tokenType, "aaaaa");
+		const res = await patchUser(signUpResponse.body.user.id, patchUserRequest, signUpResponse.body.tokenType, "aaaaa");
 
 		expect(res.status).to.equal(401);
 		expect(res.body).not.to.be.empty;
@@ -66,7 +74,7 @@ describe("get user", function() {
 
 		const anotherSignUpResponse = await signUp(signUpRequest);
 
-		const res = await getUser(signUpResponse.body.user.id, anotherSignUpResponse.body.tokenType, anotherSignUpResponse.body.accessToken);
+		const res = await patchUser(signUpResponse.body.user.id, patchUserRequest, anotherSignUpResponse.body.tokenType, anotherSignUpResponse.body.accessToken);
 
 		expect(res.status).to.equal(403);
 		expect(res.body).not.to.be.empty;
@@ -74,7 +82,7 @@ describe("get user", function() {
 	});
 
 	it("should fail due to not existing user", async function() {
-		const res = await getUser(99999, signUpResponse.body.tokenType, signUpResponse.body.accessToken);
+		const res = await patchUser(99999, patchUserRequest, signUpResponse.body.tokenType, signUpResponse.body.accessToken);
 
 		expect(res.status).to.equal(404);
 		expect(res.body).not.to.be.empty;
