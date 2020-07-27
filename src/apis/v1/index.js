8dd2b58066b6afc58f4195250b3bbeeffc768a1e
ageun <skfk0135@gmail.com>
2020-07-27 16:10:36 +0900

    Feature/sign in (#34)
    로그인 구현

--- src/apis/v1/index.js
+++ src/apis/v1/index.js
@@ -1,5 +1,6 @@
 const router = require("./router");
 
 require("./auth/sign-up")(router);
+require("./auth/sign-in")(router);
 
 module.exports = router;
--- src/test/sign-in.test.js
+++ src/test/sign-in.test.js
@@ -1,3 +1,4 @@
+const { signUp } = require("./sign-up.test");
 const chai = require("chai");
 const db = require("../models");
 const expect = require("chai").expect;
@@ -7,18 +8,19 @@ const app = require("../app");
 chai.use(chaiHttp);
 chai.should();
 
-async function signUp(signUpRequest) {
+async function signIn(signInRequest) {
 	return chai.request(app)
-		.post("/v1/auth/sign-up")
-		.send(signUpRequest);
-};
+		.post("/v1/auth/sign-in")
+		.send(signInRequest);
+}
 
 before(async function() {
 	await db.sequelize.sync({ force: true });
 });
 
-describe("sign up", function() {
+describe("sign in", function() {
 	it("should success", async function() {
+		// TODO: create user factory
 		const signUpRequest = {
 			name: "string1",
 			email: "use21@exa2mple.com",
@@ -28,33 +30,19 @@ describe("sign up", function() {
 			companyName: "string",
 			phoneNumber: "string"
 		};
+		await signUp(signUpRequest);
+		
+		const signInRequest = {
+			email: "use21@exa2mple.com",
+			password: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+		};
 
-		const res = await signUp(signUpRequest);
+		const res = await signIn(signInRequest);
 
-		expect(res.status).to.equal(201);
+		expect(res.status).to.equal(200);
 		expect(res.body).not.to.be.empty;
 		expect(res.body.accessToken).not.to.be.empty;
 		expect(res.body.refreshToken).not.to.be.empty;
 		expect(res.body.tokenType).not.to.be.empty;
 	});
-
-	it("should fail due to conflict error", async function() {
-		const signUpRequest = {
-			name: "string1",
-			email: "use21@exa2mple.com",
-			password: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
-			type: "SHIPPER",
-			telephoneNumber: "string",
-			companyName: "string",
-			phoneNumber: "string"
-		};
-
-		const res = await signUp(signUpRequest);
-
-		expect(res.status).to.equal(409);
-		expect(res.body).not.to.be.empty;
-		expect(res.body.message).not.to.be.empty;
-	});
 });
-
-exports.signUp = signUp;
