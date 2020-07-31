6dedaf25ad5b8a22492519d79c5c1bd36d3148f2
nostalgia <skfk0135@gmail.com>
2020-08-01 07:32:53 +0900

    add warehouse location model

---
+++ src/models/warehouse-location.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+	const modelName = "warehouseLocations";
+	const tableName = "warehouse_locations";
+
+	const WarehouseLocation = sequelize.define(modelName, {
+		id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+		warehouseId: { type: DataTypes.INTEGER, allowNull: false },
+		latitude: { type: DataTypes.DOUBLE, allowNull: false },
+		longitude: { type: DataTypes.DOUBLE, allowNull: false }
+	}, { tableName: tableName, timestamps: false });
+
+	return WarehouseLocation;
+}
