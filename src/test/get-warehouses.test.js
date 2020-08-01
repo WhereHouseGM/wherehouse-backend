af3a11b8d59946921910e58e0864d24b17eb430d
nostalgia <skfk0135@gmail.com>
2020-08-01 16:31:27 +0900

    테스트 코드 작성중

--- src/test/get-warehouses.test.js
+++ src/test/get-warehouses.test.js
@@ -7,28 +7,43 @@ const app = require("../app");
 chai.use(chaiHttp);
 chai.should();
 
-function buildUrl(address, minSize, maxSize) {
-	if()
+function buildUrl(query) {
+	let url = "";
+
+	if(query.address !== undefined)
+		url += "address=${address}";
+	if(query.minSize !== undefined)
+		url += "&minSize=${minSize}";
+	if(query.maxSize !== undefined)
+		url += "&maxSize=${maxSize}";
+
+	return url;
 }
-async function getWarehouses(address, minSize, maxSize) {
+
+async function getWarehouses(query) {
 	return chai.request(app)
-		.get("/v1/warehouses");
+		.get(buildUrl(query));
 }
 
 describe("get warehouses", function() {
 	before(async function() {
-		await db.sequelize.sync();
+		await db.sequelize.sync({ force: true });
 	});
 
 	it("success without any query param", async function() {
-
+		const warehouses = await getWarehouses();
 	});
 
 	it("success address query param", async function() {
-
+		const warehouses = await getWarehouses({
+			address: "경기"
+		});
 	});
 
 	it("success minSize, maxSize query param", async function() {
-
+		const warehouses = await getWarehouses({
+			minSize: 0,
+			maxSize: 100
+		});
 	});
 });
