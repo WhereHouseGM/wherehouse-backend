d649407f2b890e68f6611484a6bc83d749f615fe
nostalgia <skfk0135@gmail.com>
2020-09-18 04:45:59 +0900

    fix: sequelize db 초기화할 때 FOREIGN_KEY_CHECKS를 0으로 설정해서 데드락 발생 안함

--- test/util/setup-database.js
+++ test/util/setup-database.js
@@ -1,6 +1,9 @@
 exports.setupDatabase = async function (db) {
-	await db.sequelize.sync();
+	await db.sequelize.transaction(async t => {
+		await db.sequelize.query("SET FOREIGN_KEY_CHECKS=0", { transaction: t });
+		await db.sequelize.sync({ transaction: t});
 
-	const models = Object.keys(db.sequelize.models).map(key => db.sequelize.models[key]);
-	await Promise.all(models.map(async model => await model.destroy({ where: {} })));
+		const models = Object.keys(db.sequelize.models).map(key => db.sequelize.models[key]);
+		await Promise.all(models.map(async model => await model.truncate({ transaction: t })));
+	});
 };
